965034450baf3f246ccf98e2329d3e9101e50783
mike <mikecee0815@gmail.com>
2018-06-13 11:05:54 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Dynamic Content </title>
+</head>
+
+<body>
+	<div id="content">
+	  <h1>heading</h1>
+	  <p>heading</p>	
+	</div>
+	
+
+</body>
+
+</html>
